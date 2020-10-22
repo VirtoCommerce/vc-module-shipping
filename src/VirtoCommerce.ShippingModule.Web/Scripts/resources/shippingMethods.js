26893071db6a0daa55302679b6ea5cf8869f5073
Andrey Kozlov <yecli.github@gmail.com>
2020-10-22 15:10:14 +0500

    VP-5647: Add `getAllRegistered` resource (#13)

--- src/VirtoCommerce.ShippingModule.Web/Scripts/resources/shippingMethods.js
+++ src/VirtoCommerce.ShippingModule.Web/Scripts/resources/shippingMethods.js
@@ -1,6 +1,7 @@
 angular.module('virtoCommerce.shippingModule')
     .factory('virtoCommerce.shippingModule.shippingMethods', ['$resource', function ($resource) {
         return $resource('api/shipping', {}, {
+            getAllRegistered: { method: 'GET', isArray: true },
             search: { method: 'POST', url: 'api/shipping/search' },
             get: { url: 'api/shipping/:id' },
             update: { method: 'PUT' },
