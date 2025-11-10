1c0af0ed6a263509b3cb3b0f2917278bfedb06c3
yuskithedeveloper <yuskianywhere@gmail.com>
2025-11-10 11:13:21 -0600

    Extracted index field names constants

--- src/VirtoCommerce.ShippingModule.Data/Search/Indexed/PickupLocationDocumentBuilder.cs
+++ src/VirtoCommerce.ShippingModule.Data/Search/Indexed/PickupLocationDocumentBuilder.cs
@@ -5,6 +5,7 @@ using VirtoCommerce.SearchModule.Core.Extensions;
 using VirtoCommerce.SearchModule.Core.Model;
 using VirtoCommerce.SearchModule.Core.Services;
 using VirtoCommerce.ShippingModule.Core.Model;
+using VirtoCommerce.ShippingModule.Core.Model.Search.Indexed;
 using VirtoCommerce.ShippingModule.Core.Services;
 
 namespace VirtoCommerce.ShippingModule.Data.Search.Indexed;
@@ -14,27 +15,27 @@ public class PickupLocationDocumentBuilder(IPickupLocationService pickupLocation
 {
     public Task BuildSchemaAsync(IndexDocument schema)
     {
-        schema.AddFilterableString("StoreId");
-        schema.AddFilterableString("OuterId");
+        schema.AddFilterableString(PickupLocationIndexFields.StoreId);
+        schema.AddFilterableString(PickupLocationIndexFields.OuterId);
 
-        schema.AddFilterableBoolean("IsActive");
+        schema.AddFilterableBoolean(PickupLocationIndexFields.IsActive);
 
-        schema.AddFilterableValue("GeoLocation", null, IndexDocumentFieldValueType.GeoPoint);
+        schema.AddFilterableValue(PickupLocationIndexFields.GeoLocation, null, IndexDocumentFieldValueType.GeoPoint);
 
-        schema.AddFilterableStringAndContentString("Name");
-        schema.AddFilterableStringAndContentString("Description");
-        schema.AddFilterableStringAndContentString("WorkingHours");
-        schema.AddFilterableStringAndContentString("ContactEmail");
-        schema.AddFilterableStringAndContentString("ContactPhone");
+        schema.AddFilterableStringAndContentString(PickupLocationIndexFields.Name);
+        schema.AddFilterableStringAndContentString(PickupLocationIndexFields.Description);
+        schema.AddFilterableStringAndContentString(PickupLocationIndexFields.WorkingHours);
+        schema.AddFilterableStringAndContentString(PickupLocationIndexFields.ContactEmail);
+        schema.AddFilterableStringAndContentString(PickupLocationIndexFields.ContactPhone);
 
-        schema.AddFilterableStringAndContentString("Address_CountryCode");
-        schema.AddFilterableStringAndContentString("Address_CountryName");
-        schema.AddFilterableStringAndContentString("Address_RegionId");
-        schema.AddFilterableStringAndContentString("Address_RegionName");
-        schema.AddFilterableStringAndContentString("Address_City");
-        schema.AddFilterableStringAndContentString("Address_Line1");
-        schema.AddFilterableStringAndContentString("Address_Line2");
-        schema.AddFilterableStringAndContentString("Address_PostalCode");
+        schema.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressCountryCode);
+        schema.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressCountryName);
+        schema.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressRegionId);
+        schema.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressRegionName);
+        schema.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressCity);
+        schema.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressLine1);
+        schema.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressLine2);
+        schema.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressPostalCode);
 
         return Task.CompletedTask;
     }
@@ -57,21 +58,21 @@ public class PickupLocationDocumentBuilder(IPickupLocationService pickupLocation
     {
         var document = new IndexDocument(pickupLocation.Id);
 
-        document.AddFilterableString("StoreId", pickupLocation.StoreId);
-        document.AddFilterableString("OuterId", pickupLocation.OuterId);
+        document.AddFilterableString(PickupLocationIndexFields.StoreId, pickupLocation.StoreId);
+        document.AddFilterableString(PickupLocationIndexFields.OuterId, pickupLocation.OuterId);
 
-        document.AddFilterableBoolean("IsActive", pickupLocation.IsActive);
+        document.AddFilterableBoolean(PickupLocationIndexFields.IsActive, pickupLocation.IsActive);
 
         if (!pickupLocation.GeoLocation.IsNullOrEmpty())
         {
-            document.AddFilterableValue("GeoLocation", GeoPoint.TryParse(pickupLocation.GeoLocation), IndexDocumentFieldValueType.GeoPoint);
+            document.AddFilterableValue(PickupLocationIndexFields.GeoLocation, GeoPoint.TryParse(pickupLocation.GeoLocation), IndexDocumentFieldValueType.GeoPoint);
         }
 
-        document.AddFilterableStringAndContentString("Name", pickupLocation.Name);
-        document.AddFilterableStringAndContentString("Description", pickupLocation.Description);
-        document.AddFilterableStringAndContentString("WorkingHours", pickupLocation.WorkingHours);
-        document.AddFilterableStringAndContentString("ContactEmail", pickupLocation.ContactEmail);
-        document.AddFilterableStringAndContentString("ContactPhone", pickupLocation.ContactPhone);
+        document.AddFilterableStringAndContentString(PickupLocationIndexFields.Name, pickupLocation.Name);
+        document.AddFilterableStringAndContentString(PickupLocationIndexFields.Description, pickupLocation.Description);
+        document.AddFilterableStringAndContentString(PickupLocationIndexFields.WorkingHours, pickupLocation.WorkingHours);
+        document.AddFilterableStringAndContentString(PickupLocationIndexFields.ContactEmail, pickupLocation.ContactEmail);
+        document.AddFilterableStringAndContentString(PickupLocationIndexFields.ContactPhone, pickupLocation.ContactPhone);
 
         if (pickupLocation.Address != null)
         {
@@ -83,13 +84,13 @@ public class PickupLocationDocumentBuilder(IPickupLocationService pickupLocation
 
     protected virtual void IndexAddress(PickupLocationAddress address, IndexDocument document)
     {
-        document.AddFilterableStringAndContentString("Address_CountryCode", address.CountryCode);
-        document.AddFilterableStringAndContentString("Address_CountryName", address.CountryName);
-        document.AddFilterableStringAndContentString("Address_RegionId", address.RegionId);
-        document.AddFilterableStringAndContentString("Address_RegionName", address.RegionName);
-        document.AddFilterableStringAndContentString("Address_City", address.City);
-        document.AddFilterableStringAndContentString("Address_Line1", address.Line1);
-        document.AddFilterableStringAndContentString("Address_Line2", address.Line2);
-        document.AddFilterableStringAndContentString("Address_PostalCode", address.PostalCode);
+        document.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressCountryCode, address.CountryCode);
+        document.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressCountryName, address.CountryName);
+        document.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressRegionId, address.RegionId);
+        document.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressRegionName, address.RegionName);
+        document.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressCity, address.City);
+        document.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressLine1, address.Line1);
+        document.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressLine2, address.Line2);
+        document.AddFilterableStringAndContentString(PickupLocationIndexFields.AddressPostalCode, address.PostalCode);
     }
 }
