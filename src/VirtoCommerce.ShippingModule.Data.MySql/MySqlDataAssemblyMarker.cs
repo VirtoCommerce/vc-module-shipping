e8c01f25742b4c15c63df7f4fbbd1795e97eef03
Oleg Zhuk <zhukoo@gmail.com>
2026-03-03 14:15:05 +0200

    fix: No instantiatable types implementing IEntityTypeConfiguration were found.

---
+++ src/VirtoCommerce.ShippingModule.Data.MySql/MySqlDataAssemblyMarker.cs
@@ -0,0 +1,3 @@
+namespace VirtoCommerce.ShippingModule.Data.MySql;
+
+public class MySqlDataAssemblyMarker;
---
+++ src/VirtoCommerce.ShippingModule.Data.SqlServer/SqlServerDataAssemblyMarker.cs
@@ -0,0 +1,3 @@
+namespace VirtoCommerce.ShippingModule.Data.SqlServer;
+
+public class SqlServerDataAssemblyMarker;
